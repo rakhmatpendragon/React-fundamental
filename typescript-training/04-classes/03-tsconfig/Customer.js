6bae593709f7eb007cd9d43bf22b8fd507ce669d
rakhmatmibrahim <rakhmat23syaladin@gmail.com>
2024-12-20 22:49:45 +0700

    add example of accessors, tsconfig, parameter properties and modules

---
+++ typescript-training/04-classes/03-tsconfig/Customer.js
@@ -0,0 +1,25 @@
+"use strict";
+class Customer {
+    constructor(theFirst, theLast) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
+    }
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(value) {
+        this._firstName = value;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(value) {
+        this._lastName = value;
+    }
+}
+// let's create an instance
+let myCustomer = new Customer("Latisya", "Ibrahim");
+myCustomer.firstName = "Lycoris";
+myCustomer.lastName = "Open";
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
